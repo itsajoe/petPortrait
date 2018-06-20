f768e6555e6d0c883dd0ca2ab30e136a33963e11
itsajoe <jmusser19@gmail.com>
2018-06-20 11:06:01 -0600

    this

--- client/src/components/modal/modal.js
+++ client/src/components/modal/modal.js
@@ -1,27 +1,27 @@
-import React from "react";
-// import "./modal.css";
+// import React from "react";
+// // import "./modal.css";
 
 
-const Modal = () => {
-    return (
-<div id="myModal" class="modal fade" role="dialog">
-  <div className="modal-dialog">
+// const Modal = () => {
+//     return (
+// <div id="myModal" class="modal fade" role="dialog">
+//   <div className="modal-dialog">
 
-    <div className="modal-content">
-      <div className="modal-header">
-        <button type="button" className="close" data-dismiss="modal">&times;</button>
-        <h4 className="modal-title">Modal Header</h4>
-      </div>
-      <div className="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
+//     <div className="modal-content">
+//       <div className="modal-header">
+//         <button type="button" className="close" data-dismiss="modal">&times;</button>
+//         <h4 className="modal-title">Modal Header</h4>
+//       </div>
+//       <div className="modal-body">
+//         <p>Some text in the modal.</p>
+//       </div>
+//       <div className="modal-footer">
+//         <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+//       </div>
+//     </div>
 
-  </div>
-</div>
-)};
+//   </div>
+// </div>
+// )};
 
-export default Modal;
+// export default Modal;
